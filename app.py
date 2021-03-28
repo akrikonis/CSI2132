9f10ad84942dee71b56a04790cd1bfafa0e0e873
Alexander Krikonis <krikonis.a@gmail.com>
2021-03-28 17:49:46 -0400

    Added python database class for database connections

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+# Libraries
+from flask import Flask # Simple rest api library
+from db import DB # Custom DB class for handling all DB related connections
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
---
+++ db.py
@@ -0,0 +1,45 @@
+# Libraries
+import psycopg2
+from psycopg2 import Error
+
+# Custom DB class for handling all DB related connections
+class DB:
+    # Standard init
+    def __init__(self, user, password, host, port, database):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.database = database
+        self.connection = None
+    
+    # Connect
+    def connect(self):
+        try:
+            self.connection = psycopg2.connect(user=self.user,
+                                            password=self.password,
+                                            host=self.host,
+                                            port=self.port,
+                                            database=self.database)
+        except (Exception, Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+    
+    # For SQL statements which modify the database
+    def commit(self, command):
+        cursor = self.connection.cursor()
+        cursor.execute(command)
+        self.connection.commit()
+        cursor.close()
+
+    # For SQL statements which return data
+    def fetch(self, command):
+        cursor = self.connection.cursor()
+        cursor.execute(command)
+        data = cursor.fetchall()
+        cursor.close()
+        return data
+    
+    # End connection
+    def close(self):
+        self.connection.close()
+        
