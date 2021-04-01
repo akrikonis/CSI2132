00c7f6f14a41d7375332540a43c1b09b3774bea0
Alexander Krikonis <krikonis.a@gmail.com>
2021-04-01 15:50:38 -0400

    Completed endpoint for ParentHotelBrand

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Libraries
 import json # For json response
 import configparser # Read credentials to connect to database
-from flask import Flask # Simple rest api library
+from flask import Flask, request # Simple rest api library
 from db import DB # Custom DB class for handling all DB related connections
 
 # Get credentials and connect to database
@@ -20,9 +20,30 @@ def home():
     <p> For a full list of available querries click <a href="https://github.com/akrikonis/CSI2132">here.</a> </p>
     '''
 
-
-@app.route('/api/parentHotels', methods=['GET'])
+#Post will insert new data, get will retrieve all data, delete will delete data
+@app.route('/api/parentHotels', methods=['GET', 'POST', 'PATCH' , 'DELETE'])
 def flask_parentHotels():
-    return json.dumps(database.fetch("SELECT * FROM ParentHotelBrand;"))
+    if request.method == 'POST':
+        NumOfHotels = request.form["NumOfHotels"]
+        ParentHotelID = request.form["ParentHotelID"]
+        CentralLocation = request.form["CentralLocation"]
+        EmailAddress = request.form["EmailAddress"]
+        PhoneNumber = request.form["PhoneNumber"]
+        database.commit("INSERT INTO ParentHotelBrand VALUES ("+ParentHotelID+","+NumOfHotels+",'"+CentralLocation+"','"+EmailAddress+"','"+PhoneNumber+"');")
+        return ""
+    elif request.method == 'GET':
+        return json.dumps(database.fetch("SELECT * FROM ParentHotelBrand;"))
+    elif request.method == 'PATCH':
+        NumOfHotels = request.form["NumOfHotels"]
+        ParentHotelID = request.form["ParentHotelID"]
+        CentralLocation = request.form["CentralLocation"]
+        EmailAddress = request.form["EmailAddress"]
+        PhoneNumber = request.form["PhoneNumber"]
+        database.commit("UPDATE ParentHotelBrand SET NumOfHotels="+NumOfHotels+", CentralLocation='"+CentralLocation+"', EmailAddress='"+EmailAddress+"', PhoneNumber='"+PhoneNumber+"' WHERE ParentHotelID="+ParentHotelID+";")
+        return ""
+    elif request.method == 'DELETE':
+        ParentHotelID = request.form["ParentHotelID"]
+        database.commit("DELETE FROM ParentHotelBrand WHERE ParentHotelID="+ParentHotelID+";")
+        return ""
 
 app.run()
